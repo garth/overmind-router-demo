f85e774ac74e2849f1a1e49bbe3ee8e9683edc64
Garth Williams <garth@wi.llia.ms>
2018-08-22 15:47:24 +0200

    Infer route action typing from implemented actions

--- src/modules/router/index.ts
+++ src/modules/router/index.ts
@@ -5,13 +5,7 @@ import { homeRouted } from '../home/actions/homeRouted'
 import { personRouted } from '../people/actions/personRouted'
 import { notFoundRouted } from '../../actions/notFoundRouted'
 
-type Actions = {
-  homeRouted: typeof homeRouted
-  personRouted: typeof personRouted
-  notFoundRouted: typeof notFoundRouted
-}
-
-export const actions: Actions = {
+export const actions = {
   homeRouted: route('/', homeRouted),
   personRouted: route('/person/:personId', personRouted),
   notFoundRouted: route('/*', notFoundRouted)
--- src/modules/router/route.ts
+++ src/modules/router/route.ts
@@ -1,13 +1,16 @@
 import page from 'page'
 
-export const route = <T extends Function>(url: string, routeAction: T) => action => {
-  page(url, ({ params }) => routeAction(action)(params))
-  return action().map((_, params) => {
-    const urlWithReplacedParams = Object.keys(params || {}).reduce((currentUrl, param) => {
-      return currentUrl.replace(`:${param}`, params[param])
-    }, url)
-    page.show(urlWithReplacedParams)
-  })
-}
+type ActionBase = (action: Function) => any
+
+export const route = <T extends ActionBase>(url: string, routeAction: T): T =>
+  (action => {
+    page(url, ({ params }) => routeAction(action)(params))
+    return action().map((_, params) => {
+      const urlWithReplacedParams = Object.keys(params || {}).reduce((currentUrl, param) => {
+        return currentUrl.replace(`:${param}`, params[param])
+      }, url)
+      page.show(urlWithReplacedParams)
+    })
+  }) as T
 
 setTimeout(() => page.start(), 0)
